d27926cd685a5fab5765b0317dd905dc9b9c3216
Nicholas Amemazior <65061618+Goldac77@users.noreply.github.com>
2021-06-21 13:04:32 +0000

    Create Mars Perseverance Rover_Mission 1 & 2.py

---
+++ Mars Perseverance Rover_Mission 1 & 2.py
@@ -0,0 +1,16 @@
+#mission 1 code
+rover.forward(335)
+rover.left(90.5)
+rover.forward(330)
+rover.left(90)
+rover.forward(335)
+rover.left(90)
+rover.forward(300)
+
+
+
+#mission 2 code
+for i in range(3):
+  rover.forward(330)
+  rover.left(90)
+rover.forward(300)
